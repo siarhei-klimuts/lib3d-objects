f73c3274d6312aeef68282e9336837dcabb615c6
Siarhei Klimuts <galiaf47@gmail.com>
2016-06-13 00:06:09 +0300

    Updated library_0003 to load materials from model
    Optimized textures
    Added floor bump map

--- src/libraries/library_0003/index.js
+++ src/libraries/library_0003/index.js
@@ -2,36 +2,42 @@ var lib3d = require('lib3d');
 
 var model = require('./model.json');
 
-var map = require('./wallpaper_512x512.jpg');
-var floor = require('./floor_512x512.jpg');
-var bumpMap = require('./4268-bump.jpg');
-var img = require('./wallpaper_512x512.jpg');
+var wall = require('./img/wallpaper.jpg');
+var floor = require('./img/floor.jpg');
+var floorBumpMap = require('./img/floor_bump_map.png');
+var img = require('./img/wallpaper.jpg');
+
+var params = {
+    name: 'library_0003',
+    model: model,
+    images: {
+        wall: wall,
+        floor: floor,
+        floorBumpMap: floorBumpMap,
+        img: img
+    }
+};
+
+var textures = {
+    wallpaper: {
+        map: 'wall'
+    },
+    floor: {
+        map: 'floor',
+        bumpMap: 'floorBumpMap',
+        bumpScale: 0.001
+    }
+};
+
+var lights = [
+    new THREE.AmbientLight(0x888888),
+    new THREE.PointLight(0x888888, 1.3, 10)
+];
+lights[1].position.set(0, 2.25, 0);
 
 function register() {
-    var library = new lib3d.LibraryData({
-        name: 'library_0003',
-        model: model,
-        images: {
-            map: map,
-            floor: floor,
-            bumpMap: bumpMap,
-            img: img
-        },
-        materials: {
-            wallpaper: {
-                map: 'map',
-                shininess: 0
-            },
-            floor: {
-                map: 'floor',
-                bumpMap: 'bumpMap',
-                bumpScale: 0.003,
-                shininess: 10
-            }
-        },
-    });
-
-    lib3d.registerLibrary(library);
+    var libraryData = new lib3d.LibraryData(params, textures, lights);
+    lib3d.registerLibrary(libraryData);
 }
 
 module.exports = {
